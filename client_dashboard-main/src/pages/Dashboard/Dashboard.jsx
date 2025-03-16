e754b5e3038a003083cee4e376e2d90a3c988c9b
latapac <latashree@pacmac.co.in>
2025-03-16 10:01:54 +0530

    div structures created

--- client_dashboard-main/src/pages/Dashboard/Dashboard.jsx
+++ client_dashboard-main/src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState, useRef,useMemo } from 'react'; 
+import React, { useEffect, useState, useRef, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { getMachines, logoutService } from '../../backservice/backservice';
 import 'chart.js/auto';
-import {  FaUser, FaSignOutAlt } from 'react-icons/fa';
+import { FaUser, FaSignOutAlt } from 'react-icons/fa';
 import { Outlet } from 'react-router-dom';
 import logo from "/logo.png"
 import { useLocation } from 'react-router-dom';
@@ -14,12 +14,12 @@ function Dashboard() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [machinesList, setMachinesList] = useState([]);
   const navigate = useNavigate();
-  const {pathname} = useLocation()
+  const { pathname } = useLocation()
 
   const dropdownRef = useRef(null);
 
   const memoizedOutlet = useMemo(() => (
-    <Outlet  />
+    <Outlet />
   ), []);
 
   const handleClickOutside = (event) => {
@@ -45,7 +45,7 @@ function Dashboard() {
         .catch(() => {
           console.log('machines fetch failed');
         })
-        .finally(() => {});
+        .finally(() => { });
     }
   }, [userData?.c_id]);
 
@@ -61,84 +61,51 @@ function Dashboard() {
     if (logoutService()) {
       navigate('/login');
     }
-  };  
+  };
 
   return (
-    <div className={`min-h-screen h-auto flex flex-col md:flex-row ${isDarkMode ? 'bg-gray-900' : 'bg-gray-100'}`}>
-      {/* Sidebar */}
-      <div className={`w-full h-auto md:w-64 shadow-lg ${isDarkMode ? 'bg-gray-800' : 'bg-gray-200'}`}>
-        <div className={`p-4 flex justify-center items-center text-2xl font-serif border-b-2 mb-1 ${isDarkMode ? 'border-blue-500 text-slate-50' : 'border-blue-300 text-gray-800'}`}>
-         <img src={logo} className='h-5' alt="" /> PACMAC
-        </div>
-        <div className="p-4 flex flex-col justify-between h-screen max-h-screen">
-          <div>
-            <h1 className="text-2xl text-blue-500 font-bold p-3 hover:cursor-pointer" onClick={() => navigate('/')}>
-            {pathname=="/data"?(<i className="fa-solid fa-arrow-left"></i>):""} Machines
-            </h1>
-            <ul className="space-y-2">
-              {machinesList.length > 0 ? (
-                machinesList.map((element) => (
-                  <li
-                    key={element.serial_number}
-                    onClick={() => {
-                      navigate(`/data?serial_number=${element.serial_number}`);
-                    }}
-                    className={`cursor-pointer p-2 rounded transition duration-200 ${isDarkMode ? 'bg-blue-600 text-white' : 'bg-blue-500 text-white'}`}
-                  >
-                    {element.serial_number}
-                  </li>
-                ))
-              ) : (
-                <div className={isDarkMode ? 'text-white' : 'text-gray-800'}>No machines available</div>
-              )}
-            </ul>
+    <>
+      <div className='bg-gray-200 h-auto min-h-screen overflow-hidden'>
+        <div>
+          <div className='bg-blue-400 h-9'>
+            <img src="" alt="" />
+            <p>PACMAC</p>
           </div>
         </div>
-      </div>
-
-      {/* Main Content */}
-      <div className="flex-1 p-4 md:p-8">
-        {/* Header */}
-        <div className="flex flex-col md:flex-row justify-between items-baseline mb-8">
-        <div className='text-3xl font-bold text-blue-500  border-b-2  pb-3'>Machine Status</div>
-          <h1 className={`text-4xl font-bold flex items-center ${isDarkMode ? 'text-blue-500' : 'text-blue-600'}`}></h1>
-          <div className="flex items-center space-x-4 mt-4 md:mt-0">
-
-              
-            {/* Welcome Message */}
-            <div className={`p-3 rounded-lg shadow-sm flex items-center ${isDarkMode ? 'bg-gray-700' : 'bg-gray-300'}
-             hover:drop-shadow-xl`}>
-              <span className={`mr-2 text-lg ${isDarkMode ? 'text-gray-300' : 'text-gray-800'}`}>Welcome,</span>
-              <span className={`font-semibold text-lg ${isDarkMode ? 'text-blue-500' : 'text-blue-600'}`}>{userData?.username}</span>
-            </div>
-
-            {/* Logout Dropdown */}
-            <div className="relative" ref={dropdownRef}>
-              <button onClick={toggleDropdown} className="flex items-center focus:outline-none  hover:shadow-cyan-500">
-                <FaUser className={`text-2xl ${isDarkMode ? 'text-blue-500' : 'text-blue-600'}`} />
-              </button>
-              {isDropdownOpen && (
-                <div className={`absolute right-0 mt-2 w-48 rounded-md shadow-lg ${isDarkMode ? 'bg-gray-700' : 'bg-white'}`}>
-                  <div className="py-1">
-                    <button
-                      onClick={handleLogout}
-                      className={`block w-full px-4 py-2 text-sm ${isDarkMode ? 'text-gray-300 hover:bg-gray-600' : 'text-gray-800 hover:bg-gray-200'}`}
-                    >
-                      <FaSignOutAlt className="inline-block mr-2" /> Logout
-                    </button>
-                  </div>
-                </div>
-              )}
+          <div className='flex flex-row justify-evenly bg-gray-200 mt-1'>
+            <div className='bg-white  w-[30vh] flex justify-center '>Home</div>
+            <div className='bg-white  w-[30vh] flex justify-center '>Analytics</div>
+            <div className='bg-white  w-[30vh] flex justify-center '>Devices</div>
+            <div className='bg-white  w-[30vh] flex justify-center '>Reports</div>
+            <div className='bg-white  w-[30vh] flex justify-center '>Status</div>
+            <div className='bg-blue-500 rounded-sm text-white  w-[20vh] flex justify-center '>Sync</div>
+          </div>
+          <div className='bg-gray-200 h-6'></div>
+          <div className=' flex flex-row w-screen h-36'>
+            <div className='w-60  h-46 bg-white'></div>
+            <div className='flex flex-col w-full gap-3'>
+              <div className='flex flex-row justify-evenly'>
+                <div className='bg-white w-62 h-22'>div1</div>
+                <div className='bg-white w-62 h-22'>div2</div>
+                <div className='bg-white w-62 h-22'>div3</div>
+                <div className='bg-white w-62 h-22'>div4</div>
+              </div>
+              <div className='flex flex-row justify-evenly'>
+                <div className='bg-white w-62 h-22'>div5</div>
+                <div className='bg-white w-62 h-22'>div6</div>
+                <div className='bg-white w-62 h-22'>div7</div>
+                <div className='bg-white w-62 h-22'>div8</div>
+              </div>
             </div>
           </div>
-        </div>
-
-        {/* Machine Details */}
-        
-         {memoizedOutlet}
-       
+          <div className='flex flex-row  w-screen h-40 gap-7 mt-16 ml-7'>
+            <div className='bg-white w-[46vw] h-84 flex'></div>
+            <div className='bg-white w-[46vw] h-84 flex'></div>
+          </div>
+          
       </div>
-    </div>
+      
+    </>
   );
 }
 
--- client_dashboard-main/src/pages/Dashboard/components/MachindeData.jsx
+++ client_dashboard-main/src/pages/Dashboard/components/MachindeData.jsx
@@ -119,110 +119,11 @@ function MachindeData() {
 
   return (
     <>
-     <div className='flex min-w-full justify-between'>
-     <button
-          className={`bg-blue-600 text-white p-2.5 rounded-lg`}>Active User :{user}</button>
-        <button
-          onClick={() => { navigate("/audit?serial_number="+serialNumber) }}
-          className={`bg-blue-600 text-white p-2.5  rounded-lg`}>Audit Trail</button>
-      </div>
-      <div className={`rounded-lg shadow-md p-3 mb-4 ${isDarkMode ? 'bg-gray-800' : 'bg-white'} h-28 `}>
-        <h6 className={`text-xl font-bold mb-1.5 flex justify-between items-center ${isDarkMode ? 'text-blue-500' : 'text-blue-600'}`}>
-          <span role="img" aria-label="chart" className={isDarkMode ? 'text-blue-500' : 'text-blue-600'}>
-            Serial No. {machineData?.serial_number}{' '}
-          </span>
-          <span>
-            Status :{' '}
-            <span className={getStatusColor(mstatus[Number(machineData?.d?.status)])}>
-              {mstatus[Number(machineData?.d?.status)]}
-            </span>
-          </span>
-        </h6>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          <div className={`p-2.5 rounded-lg shadow-sm flex flex-row justify-evenly h-10 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-100'}`}>
-            <p className={`font-medium text-18   ${isDarkMode ? 'text-gray-300' : 'text-gray-800'}`}>Current Speed</p>
-            <p className={`text-xl font-semibold ${isDarkMode ? 'text-blue-500' : 'text-blue-600'}`}>{machineData?.d?.current_speed}</p>
-          </div>
-          <div className={`p-2.5 rounded-lg shadow-sm flex flex-row justify-evenly h-10 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-100'}`}>
-            <p className={`font-medium text-18 ${isDarkMode ? 'text-gray-300' : 'text-gray-800'}`}>Batch Number</p>
-            <p className={`text-xl font-semibold ${isDarkMode ? 'text-blue-500' : 'text-blue-600'}`}>{machineData?.d?.Batch_Number}</p>
-          </div>
-          <div className={`p-2.5 rounded-lg shadow-sm flex flex-row justify-evenly h-10 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-100'}`}>
-            <p className={`font-medium text-18  ${isDarkMode ? 'text-gray-300' : 'text-gray-800'}`}>Recipe Name</p>
-            <p className={`text-xl font-semibold ${isDarkMode ? 'text-blue-500' : 'text-blue-600'}`}>{machineData?.d?.Reciepe_Name}</p>
-          </div>
-        </div>
-      </div>
-      <div className={`rounded-lg shadow-md p-3 mb-4 ${isDarkMode ? 'bg-gray-800' : 'bg-white'} h-28`}>
-        <h6 className={`text-xl font-bold mb-1.5 flex justify-between items-center ${isDarkMode ? 'text-blue-500' : 'text-blue-600'}`}>
-          <span role="img" aria-label="chart" className={isDarkMode ? 'text-blue-500' : 'text-blue-600'}>
-            Production Details
-          </span>
-        </h6>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          <div className={`p-2.5 rounded-lg shadow-sm flex flex-row justify-evenly h-10 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-100'}`}>
-            <p className={`font-medium text-18 ${isDarkMode ? 'text-gray-300' : 'text-gray-800'}`}>Total Production</p>
-            <p className={`text-xl font-semibold ${isDarkMode ? 'text-blue-500' : 'text-blue-600'}`}>{machineData?.d?.Total_Production}</p>
-          </div>
-          <div className={`p-2.5 rounded-lg shadow-sm flex flex-row justify-evenly h-10 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-100'}`}>
-            <p className={`font-medium text-18 ${isDarkMode ? 'text-gray-300' : 'text-gray-800'}`}>Good Production</p>
-            <p className={`text-xl font-semibold ${isDarkMode ? 'text-blue-500' : 'text-blue-600'}`}>{machineData?.d?.Good_Count}</p>
-          </div>
-          <div className={`p-2.5 rounded-lg shadow-sm flex flex-row justify-evenly h-10 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-100'}`}>
-            <p className={`font-medium text-18 ${isDarkMode ? 'text-gray-300' : 'text-gray-800'}`}>Bad Production</p>
-            <p className={`text-xl font-semibold ${isDarkMode ? 'text-blue-500' : 'text-blue-600'}`}>{machineData?.d?.Reject_Counters}</p>
-          </div>
-        </div>
-      </div>
-      <div className={`rounded-lg shadow-md p-3 mb-1.5 ${isDarkMode ? 'bg-gray-800' : 'bg-white'} h-28`}>
-        <h6 className={`text-xl font-bold mb-1.5 flex justify-between items-center ${isDarkMode ? 'text-blue-500' : 'text-blue-600'}`}>
-          <span role="img" aria-label="chart" className={isDarkMode ? 'text-blue-500' : 'text-blue-600'}>
-            OEE Details
-          </span>
-        </h6>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 ">
-          <div className={`p-2.5 rounded-lg shadow-sm flex flex-row justify-evenly h-10 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-100'}`}>
-            <p className={`font-medium text-18 ${isDarkMode ? 'text-gray-300' : 'text-gray-800'}`}>Performance</p>
-            <p className={`text-xl font-semibold ${isDarkMode ? 'text-blue-500' : 'text-blue-600'}`}>{performance}</p>
-          </div>
-          <div className={`p-2.5 rounded-lg shadow-sm flex flex-row justify-evenly h-10 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-100'}`}>
-            <p className={`font-medium text-18 ${isDarkMode ? 'text-gray-300' : 'text-gray-800'}`}>Availability</p>
-            <p className={`text-xl font-semibold ${isDarkMode ? 'text-blue-500' : 'text-blue-600'}`}>{availability}</p>
-          </div>
-          <div className={`p-2.5 rounded-lg shadow-sm flex flex-row justify-evenly h-10 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-100'}`}>
-            <p className={`font-medium text-18 ${isDarkMode ? 'text-gray-300' : 'text-gray-800'}`}>Quality</p>
-            <p className={`text-xl font-semibold ${isDarkMode ? 'text-blue-500' : 'text-blue-600'}`}>{quality}</p>
-          </div>
-        </div>
-      </div>
-      {/* Half-Donut Charts and Needle Indicator */}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        {/* Good Count vs Rejected Count */}
-        <div className={`rounded-lg shadow-md p-6 flex flex-col justify-center items-center ${isDarkMode ? 'bg-gray-800' : 'bg-white'}`} style={{ height: '100%' }}>
-          <h3 className={`text-2xl font-bold mb-4 ${isDarkMode ? 'text-blue-500' : 'text-blue-600'}`}>Production Overview</h3>
-          <div className="relative min-h-80">
-            <Doughnut data={productionData} options={options} />
-            <div className="absolute inset-0 flex items-center justify-center">
-              <span className={`text-3xl font-bold ${isDarkMode ? 'text-blue-500' : 'text-blue-600'}`}>
-                {machineData?.d?.Total_Production || 0}
-              </span>
-            </div>
-          </div>
-        </div>
+    <div>
+      <div className='bg-indigo-400 w-3 h-6'>
 
-        {/* OEE Chart with Performance, Availability, and Quality */}
-        <div className={`rounded-lg shadow-md p-6 flex flex-col items-center justify-center ${isDarkMode ? 'bg-gray-800' : 'bg-white'}`} style={{ height: '100%' }}>
-          <h3 className={`text-2xl font-bold mb-4 ${isDarkMode ? 'text-blue-500' : 'text-blue-600'}`}>OEE</h3>
-          <div className="relative" style={{ height: '300px' }}>
-            <Doughnut data={oeeData} options={oeeOptions} />
-            <div className="absolute inset-0 flex items-center justify-center">
-              <span className={`text-3xl font-bold ${isDarkMode ? 'text-blue-500' : 'text-blue-600'}`}>
-                {oeeValue}%
-              </span>
-            </div>
-          </div>
-        </div>
       </div>
+    </div>
     </>
   );
 }
